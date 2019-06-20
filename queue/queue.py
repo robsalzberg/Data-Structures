bc2be4d7e95e1fc8f0ba26a81e39c98539a89215
Rob Salzberg <robsalzberg@gmail.com>
2019-06-19 21:10:36 -0400

    Added item to back of the queue

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,15 @@
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements?
+        self.storage = list()
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        self.storage.insert(0, item)
 
-  def len(self):
-    pass
+    def dequeue(self):
+        pass
+
+    def len(self):
+        pass
