8015f6dc2d260bea2acddd791faef2476b0a3e0f
Rob Salzberg <robsalzberg@gmail.com>
2019-06-27 20:58:45 -0400

    max_heap.py bubble_up helper method

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -14,8 +14,25 @@ class Heap:
   def get_size(self):
     pass
 
-  def _bubble_up(self, index):
-    pass
+  # Note that index is the spot in the storage array that
+    # the element we just appended to the storage array 
+    # currently occupies
+    def _bubble_up(self, index):
+        # 1. Check to see if the index is greater than zero
+        # 2. Grab parent index
+        # 3. Check if current value is greater than or less than parent value
+        #     a. If current is greater than
+        #     b. Swap
+        # 4. If current is lesser than parent
+        #     a. Leave it alone - break
+        while index > 0:
+            parent = (index - 1) // 2
+
+            if self.storage[index] > self.storage[parent]:
+                self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+                index = parent #Updating your new index
+            else:
+                break
 
   def _sift_down(self, index):
     pass
